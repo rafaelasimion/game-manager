3cedeba2038ddc9ddd8874612a34fc4dc2b9c2f2
Rafaela <rafaelasimion06@gmail.com>
2026-05-25 09:44:43 -0300

    fix: corrig next.config.mjs para suporte ao export do Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "game-manager";
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
