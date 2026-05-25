a289d8d9ed8a26435f92e4ad99b609a07a896f86
Rafaela <rafaelasimion06@gmail.com>
2026-05-25 09:45:24 -0300

    Updates

---
+++ _next/static/amb87htN31mDnI2CfIZhR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/game-manager//_next/:path+",
+        "destination": "/game-manager/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
